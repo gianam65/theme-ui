d4b5101cffb19ceff3aa9fb2b25357b6bdada1ea
gianam65 <gianam65012001@gmail.com>
2024-04-27 11:03:50 +0700

    update: add instruction use image

--- src/pages/home-page/home-page.jsx
+++ src/pages/home-page/home-page.jsx
@@ -1,7 +1,13 @@
 import "./home-page.scss";
+import qrCode from "../../assets/images/test.png";
 
 const HomePage = () => {
-  return <div>This is home page</div>;
+  return (
+    <div>
+      This is home page
+      <img src={qrCode} alt="" />
+    </div>
+  );
 };
 
 export default HomePage;
